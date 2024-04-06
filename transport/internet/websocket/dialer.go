06ba768b42c9d7cc76dd88b4a60775e399596b82
dharak36 <82281800+dharak36@users.noreply.github.com>
2024-04-06 22:10:03 +0700

    Update dialer.go

--- transport/internet/websocket/dialer.go
+++ transport/internet/websocket/dialer.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gorilla/websocket"
+	"github.com/dharak36/websocket"
 	"github.com/xtls/xray-core/common"
 	"github.com/xtls/xray-core/common/net"
 	"github.com/xtls/xray-core/common/platform"
